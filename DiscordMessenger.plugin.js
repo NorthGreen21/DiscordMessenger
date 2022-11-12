76df11f2af8640d0c32d65d59cf0749290c1bcc4
Demetrius Hiscox <demetriushiscox123@gmail.com>
2022-11-11 21:44:07 -0700

    Adding initial plugin file

---
+++ DiscordMessenger.plugin.js
@@ -0,0 +1,14 @@
+/**
+ * @name DiscordMessenger
+ * @author NorthGreen21
+ * @description A plugin for BetterDiscord that reduces the size of the Discord window and allows for a more compact and out-of-your-way approach to a messenger.
+ * @version 0.1
+ */
+
+module.exports = class DiscordMessenger {
+    start() {
+        console.log("Hello world!");
+    }
+
+    stop() {}
+}
