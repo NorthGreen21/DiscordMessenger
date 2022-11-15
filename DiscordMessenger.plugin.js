e2f5eeddf74dbe2b465884a765513583eb861945
Demetrius Hiscox <demetriushiscox123@gmail.com>
2022-11-14 21:40:41 -0700

    Spacing

--- DiscordMessenger.plugin.js
+++ DiscordMessenger.plugin.js
@@ -18,13 +18,16 @@
         var button = document.createElement('img');
         button.setAttribute('src', 'https://raw.githubusercontent.com/NorthGreen21/DiscordMessenger/main/dmsized.png');
 		button.setAttribute('id', "messenger");
+
         var location = document.getElementsByClassName('listItem-3SmSlK')[0];
         location.insertAdjacentElement('afterend', button);
+
 		button.style.height = "23px";
 		button.style.width = "36px";
 		button.style.marginLeft = "18px";
 		button.style.marginTop = "5px";
 		button.style.marginBottom = "10px";
+        
 		button.addEventListener('mouseover', hoverOver);
 		button.addEventListener('click', loadMessenger);
     }
