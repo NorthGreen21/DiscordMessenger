0c443b6533e5ae728d85545b858de3508047ef19
Demetrius Hiscox <demetriushiscox123@gmail.com>
2022-11-14 21:39:50 -0700

    New design with image button

--- DiscordMessenger.plugin.js
+++ DiscordMessenger.plugin.js
@@ -7,44 +7,26 @@
 
  module.exports = class DiscordMessenger {
     start() {
-		function buttonHoverOver() {
-			button.style.backgroundColor = '#40A05E';
+		function hoverOver() {
+			button.style.cursor = 'pointer';
 		}
 
-		function buttonHoverOut() {
-			button.style.backgroundColor = 'var(--background-primary)';
+		function loadMessenger() {
+			console.log("Loaded!");
 		}
 
-		function buttonDown() {
-			button.style.boxShadow = 'none';
-			button.style.transform = 'translate(1px, 1px)';
-		}
-
-		function buttonUp() {
-			button.style.boxShadow = '3px 3px var(--background-floating)';
-			button.style.transform = 'translate(0px, 0px)';
-		}
-
-        var button = document.createElement('button');
-        button.setAttribute('id', "messenger");
+        var button = document.createElement('img');
+        button.setAttribute('src', 'https://raw.githubusercontent.com/NorthGreen21/DiscordMessenger/main/dmsized.png');
+		button.setAttribute('id', "messenger");
         var location = document.getElementsByClassName('listItem-3SmSlK')[0];
         location.insertAdjacentElement('afterend', button);
-
-		button.style.width = '42px';
-		button.style.height = '12px';
-		button.style.margin = '15px';
-		button.style.marginTop = '3px';
-		button.style.marginBottom = '10px';
-		button.style.borderRadius = '3px';
-		button.style.border = '1px solid var(--background-secondary)';
-		button.style.backgroundColor = 'var(--background-primary)';
-		button.style.boxShadow = '3px 3px var(--background-floating)';
-		button.style.transition = 'background-color .1s ease-out';
-
-		button.addEventListener('mouseover', buttonHoverOver);
-		button.addEventListener('mouseout', buttonHoverOut);
-		button.addEventListener('mousedown', buttonDown);
-		button.addEventListener('mouseup', buttonUp);
+		button.style.height = "23px";
+		button.style.width = "36px";
+		button.style.marginLeft = "18px";
+		button.style.marginTop = "5px";
+		button.style.marginBottom = "10px";
+		button.addEventListener('mouseover', hoverOver);
+		button.addEventListener('click', loadMessenger);
     }
 
     stop() {
